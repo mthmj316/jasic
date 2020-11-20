c455f539bb99afc17cd6276c9779a1879ac551af
mthmj316 <thoma_markus@web.de>
2020-11-20 10:14:10 +0100

    initial load

---
+++ jasic/src/main/java/de/mthoma/web/jasic/JasicApplication.java
@@ -0,0 +1,13 @@
+package de.mthoma.web.jasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasicApplication.class, args);
+	}
+
+}
---
+++ jasic/src/test/java/de/mthoma/web/jasic/JasicApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mthoma.web.jasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
