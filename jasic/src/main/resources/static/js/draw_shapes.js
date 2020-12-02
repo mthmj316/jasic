b3c55e299ce88c5b8b572f63b7cde92faa096f99
mthmj316 <thoma_markus@web.de>
2020-12-02 09:23:59 +0100

    Draw triangle

--- jasic/src/main/resources/static/js/draw_shapes.js
+++ jasic/src/main/resources/static/js/draw_shapes.js
@@ -1,7 +1,7 @@
 /**
  * 
  */
-export function drawTriangle(context,p1_x, p1_y, p2_x, p2_y, p3_x, p3_y){
+function drawTriangle(context,p1_x, p1_y, p2_x, p2_y, p3_x, p3_y){
 	
 	drawLine(context, p1_x, p1_y, p2_x, p2_y);
 	drawLine(context, p2_x, p2_y, p3_x, p3_y);
@@ -16,7 +16,7 @@ export function drawTriangle(context,p1_x, p1_y, p2_x, p2_y, p3_x, p3_y){
  * @param p2_x x-coordinate of the end point of line
  * @param p2_y y-coordinate of the end point of line
  */
-export function drawLine(context, p1_x, p1_y, p2_x, p2_y){
+function drawLine(context, p1_x, p1_y, p2_x, p2_y){
 	context.beginPath();
 	context.moveTo(p1_x, p1_y);
 	context.lineTo(p2_x, p2_y);
