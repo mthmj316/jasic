215c456860063ad4b94c1981693aca690f3d3292
mthmj316 <thoma_markus@web.de>
2021-01-07 22:57:59 +0100

    thymleaf select form

--- jasic/src/main/java/de/mthoma/web/jasic/controller/TableOfContentsController.java
+++ jasic/src/main/java/de/mthoma/web/jasic/controller/TableOfContentsController.java
@@ -3,14 +3,32 @@ package de.mthoma.web.jasic.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import de.mthoma.web.jasic.entities.Chapter;
 
 @Controller
 public class TableOfContentsController {
+	
+//	@RequestMapping(value="perfrom_create_entry", method = RequestMethod.POST)
+	@PostMapping("/perform_create_entry")
+	public String performCreateEntry(@ModelAttribute  Chapter selectParent, Model model) {
+		//@RequestParam(value="idNumber") String idNumber , @RequestParam(value="applicationId") String applicationId
+		
+		System.out.println("Chapter parent: " + selectParent);
+		
+		return "table_of_contents/table_of_contents";
+	}
 
 	@GetMapping(value = "/table_of_contents/create_entry")
 	public String createEntry(Model model) {
