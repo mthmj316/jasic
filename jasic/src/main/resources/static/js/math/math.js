3d37b73d26c01e714fa377aeac07649566f7210a
mthmj316 <thoma_markus@web.de>
2021-02-22 08:56:36 +0100

    math.js implementation started

---
+++ jasic/src/main/resources/static/js/math/math.js
@@ -0,0 +1,52 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+export function differentiateWithRespectTo(expression, variable){
+	
+}
+
+//#####################################################
+// private functions #########################################
+//#####################################################
+
+/*
+ * Splits the given expression in its summands.
+ * 
+ * e.g.: 2x² - 2x + 3 -> ["2x²", "-2x", "3"]
+ * 
+ * It is not checked if the expression is valid.
+ */
+function splitMathSumExpression(expression){
+	
+}
+
+/*
+ * Reduces the given fraction.
+ * Only simple fractiosn like "2/4" are supported.
+ * "Double fraction" like "(2/4)/(3/6) are not supported.
+ * Variables in the fraction are not supported.
+ * It is not checked if the fraction is valid.
+ */
+function reduceFraction(fraction){
+	
+}
+
+/*
+ * Mult
+ */
+function multiplyFractions(fraction1, fraction2){
+	
+}
+
+function multiply(multiplier1, multiplier2){
+	
+}
+
+function isNumberOrFraction(term){
+	
+}
+
+function differentiatePartial(partialExpression, variable){
+	
+}
