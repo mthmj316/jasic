f86fb017c70df1befc09a0fce20fcee0f103fd3e
mthmj316 <64063837+mthmj316@users.noreply.github.com>
2021-05-11 09:08:08 +0200

    static fragments: header, footer and navigation bar

---
+++ jasic/src/main/java/de/mthoma/jasic/web/controller/NavigationController.java
@@ -0,0 +1,22 @@
+package de.mthoma.jasic.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NavigationController extends JasicController{
+	
+	private static final String NO_SUBJECT_AVAILABLE_URL = "/no_subject_available";
+	
+	private static final String NO_SUBJECT_AVAILABLE_PAGE = "no_subject_available";
+	
+	private static final String MAIN_PAGE = "main";
+	
+	@GetMapping(value = NO_SUBJECT_AVAILABLE_URL)
+	public String getNoSubjectAvailablePage(Model model) {
+		
+		return MAIN_PAGE;
+	}
+	
+}
