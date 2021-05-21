1dc2ae01530f0aa0e56f3bf642ca6601da0ab7bd
mthmj316 <64063837+mthmj316@users.noreply.github.com>
2021-05-21 12:25:37 +0200

    example path finished and example average speed started

--- jasic/src/main/java/de/mthoma/jasic/web/controller/ExampleController.java
+++ jasic/src/main/java/de/mthoma/jasic/web/controller/ExampleController.java
@@ -9,6 +9,15 @@ public class ExampleController {
 	public static final String KINEMATICS_RECTILINEAR_MOTION_PATH_URL = "/examples/mechanics/kinematics/rectilinear_motion_path";
 	public static final String KINEMATICS_RECTILINEAR_MOTION_PATH_PAGE = "examples/mechanics/kinematics/rectilinear_motion_path";
 	
+	public static final String KINEMATICS_RECTILINEAR_MOTION_AVERAGE_SPEED_URL = "/examples/mechanics/kinematics/rectilinear_motion_average_speed";
+	public static final String KINEMATICS_RECTILINEAR_MOTION_AVERAGE_SPEED_PAGE = "examples/mechanics/kinematics/rectilinear_motion_average_speed";
+	
+	@GetMapping(value = KINEMATICS_RECTILINEAR_MOTION_AVERAGE_SPEED_URL)
+	public String kinematicsRectilinearMotionAverageSpeed() {
+		
+		return KINEMATICS_RECTILINEAR_MOTION_AVERAGE_SPEED_PAGE;
+	}
+	
 	@GetMapping(value = KINEMATICS_RECTILINEAR_MOTION_PATH_URL)
 	public String kinematicsRectilinearMotionPath() {
 		
