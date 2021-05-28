f0ccdbf182d50b66462a998a111a95c5f4507276
mthmj316 <64063837+mthmj316@users.noreply.github.com>
2021-05-28 14:45:19 +0200

    slide_controller finished

--- jasic/src/main/resources/static/js/controller/common/slide_controller.js
+++ jasic/src/main/resources/static/js/controller/common/slide_controller.js
@@ -12,64 +12,155 @@
  * - The index slide must have the id slide_0
  */
 
-window.jumpsTo = function jumpsTo(document, slideId){
-	
+/**
+ * Sets the slide with given slideId visble and all others invisible.
+ */
+window.jumpsTo = function jumpsTo(document, slideId) {
+
+	setSlideVisibility(document, slideId);
 }
 
-window.next = function next(document){
-	
+/**
+ * Makes the next slide in row visible. If there is no next slide,
+ * the index slide will be made visible.
+ */
+window.next = function next(document) {
+
+	// Get the slide id number of the currently visible slide
+	const slideId = getVisibleSlideId(document);
+
+	const extractedSlideIdNr = extractIdNumber(slideId);
+
+	// Increment the slide id number by 1 and check if the number exists. 
+	// If so compound the slide of the new visible slide, otherwise the 
+	// set new slide id to the index page (slide_0).
+	const newSlideIdNr = extractedSlideIdNr + 1;
+
+	var newSlideId;
+
+	if (newSlideIdNr < getSlideCount(document)) {
+		newSlideId = compoundSlideId(newSlideIdNr);
+	} else {
+		newSlideId = "slide_0";
+	}
+
+	setSlideVisibility(document, newSlideId);
 }
 
-window.previsous = function previsous(document){
-	
+/**
+ * Makes the slide before the current slide visible.
+ * If the first slide is reached the last slide will be made visible.
+ */
+window.previous = function previsous(document) {
+
+	//Get the slide id number of the currently visible slide
+	const slideId = getVisibleSlideId(document);
+
+	const extractedSlideIdNr = extractIdNumber(slideId);
+
+	// Check if the number of the currently selected slide is 0.
+	// If so set the number to the last slide , if not decrement the extracted number.
+	var newSlideIdNr;
+	if (extractedSlideIdNr == 0) {
+		newSlideIdNr = getSlideCount(document) - 1;
+	} else {
+		newSlideIdNr = extractedSlideIdNr - 1;
+	}
+
+	const newSlideId = compoundSlideId(newSlideIdNr);
+
+	setSlideVisibility(document, newSlideId);
 }
 
-window.index = function index(document){
-	
+/**
+ * Makes the index slide visible an all others invisible.
+ */
+window.index = function index(document) {
+
+	setSlideVisibility(document, "slide_0");
 }
 
 // --------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------
 // Private methods ----------------------------------------------------------------
+/*
+* Iterates the slides with in the slide container.
+* It returns the id of the first visible slide.
+*/
+function getVisibleSlideId(document) {
 
-function getVisibleSlideId(document){
-	
+	//Get child elements of the slide container.
+	const slides = document.getElementById("slide_container").children;
+
+	//Iterate over each slide and check if the slide is visible, and if so return its ID
+	var slideId;
+
+	var slide;
+	for (var i = 0; i < slides.length; i++) {
+
+		slide = slides[i];
+
+		if (slide.style.display == "block") {
+			slideId = slide.id;
+			break;
+		}
+	}
+
+	return slideId;
 }
 
-function extractIdNumber(slideId){
-	
+/*
+* Extracts from the given slide id the slide id number:
+* slideId == slide_9 -> return 9
+*/
+function extractIdNumber(slideId) {
+
+	//Split slide id at "_"
+	const splitSlideId = slideId.split(/_/);
+
+	//Convert the second part of the slide id to int
+	const slideIdNr = parseInt(splitSlideId[1]);
+
+	return slideIdNr;
 }
 
-
-function compoundSlideId(slideIdNr){
-	
+/*
+* Creates an slide Id using the given slideIdNr as follows:
+* slideId = "slide_" + slideIdNr;
+*/
+function compoundSlideId(slideIdNr) {
+	return "slide_" + slideIdNr;
 }
 
 /*
  * Sets the style attribute display of the slide with the given
  * id idOfVisibleSlide to "block", and of all other to "none".
  */
-function setSlideVisibility(document, idOfVisibleSlide){
-	
+function setSlideVisibility(document, idOfVisibleSlide) {
+
 	//Get child elements of the slide container.
-	const slides = document.getElementById("slide_container").childNodes;
-	
+	const slides = document.getElementById("slide_container").children;
+
 	//Iterate over each slide and check if the slide must be visible,
 	//at set the display attribute correspondingly.
-	slides.forEach(function (slide){
-		
-		if(slide.id == idOfVisibleSlide){
+
+	var slide;
+	for (var i = 0; i < slides.length; i++) {
+
+		slide = slides[i];
+
+		if (slide.id == idOfVisibleSlide) {
 			slide.style.display = "block";
 		} else {
 			slide.style.display = "none";
 		}
-	});
+	}
 }
 
 /*
  * Returns the number of child elements within the slide container.
  */
 function getSlideCount(document) {
-	
-	return document.getElementById("slide_container").childNodes.length;
+
+	return document.getElementById("slide_container").children.length;
 }
